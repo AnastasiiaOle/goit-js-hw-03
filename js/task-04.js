b9c5b5ce7eaaed2107cc92499500ed304accd83e
Anastasiia Oleinikova <anastasiiaole@gmail.com>
2020-12-13 21:32:06 +0100

    updated with tasks

---
+++ js/task-04.js
@@ -0,0 +1,33 @@
+const countTotalSalary = function(employees) {
+
+    const salaries = Object.values(employees);
+    console.log(salaries);
+  
+    let sum = 0;
+  
+    for (const salary of salaries) {
+        sum += salary;
+    };
+    return sum;
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
